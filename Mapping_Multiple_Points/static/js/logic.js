c1f4299fd73f20b7e1515005aa86cc74ce248bf3
Kola Oyedele <kolaoyedele@hotmail.com>
2021-03-11 16:14:51 -0500

    multiple circle markers and popups added

--- Mapping_Multiple_Points/static/js/logic.js
+++ Mapping_Multiple_Points/static/js/logic.js
@@ -16,9 +16,13 @@ let map = L.map('mapid').setView([34.05, -108.24], 5);
 let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
+cityData.forEach(function(city) {
     console.log(city);
-    L.marker(city.location).addTo(map);
+    L.circleMarker(city.location, {
+        radius: city.population/100000
+    })
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+    .addTo(map);
 });
 
 // We create the tile layer that will be the background of our map.

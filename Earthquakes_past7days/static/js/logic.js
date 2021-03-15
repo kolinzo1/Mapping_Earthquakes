b85cee9f9e8556f9754357e0e63e6b3e7c0827b3
Kola Oyedele <kolaoyedele@hotmail.com>
2021-03-15 08:05:40 -0400

    adding updates for past 7 days earthquake branch

---
+++ Earthquakes_past7days/static/js/logic.js
@@ -0,0 +1,87 @@
+// Add console.log to check to see if our code is working.
+console.log("working");
+
+
+// // Create the map object with a center and zoom level.
+// let map = L.map("mapid", {
+//     center: [40.7, -94.5],
+//     zoom: 4
+//   });
+
+
+
+// // Add a marker to the map for Los Angeles, California.
+// L.circle([34.0522, -118.2437], {
+//     color: 'black',
+//     fillColor: '#FFFF99',
+//     fillOpacity: 0.5,
+//     radius: 300
+// }).addTo(map);
+
+// // Get data from cities.js
+// let cityData = cities;
+
+// // Loop through the cities array and create one marker for each city.
+// cityData.forEach(function(city) {
+//     console.log(city);
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//     .addTo(map);
+// });
+
+// We create the tile layer that will be the background of our map.
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+
+// We create the dark view tile layer that will be an option for our map.
+let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+// Create a base layer that holds both maps.
+let baseMaps = {
+   Streets: streets,
+   Satellite_Streets: satelliteStreets
+  };
+
+
+// Create the map object with center, zoom level and default layer.  
+let map = L.map('mapid', {
+    center: [43.7, -79.3],
+    zoom: 11,
+    layers: [streets, satelliteStreets]
+})
+
+  // Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
+
+// Accessing the airport GeoJSON URL
+let torontoHoods = "https://raw.githubusercontent.com/kolinzo1/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+
+// Create a style for the lines.
+let myStyle = {
+    color: "blue",
+    weight: 1,
+    fillColor: "yellow"
+  
+}
+
+// Grabbing our GeoJSON data.
+d3.json(torontoHoods).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJson(data, {
+    style: myStyle,
+    onEachFeature: function(feature, layer) {
+      layer.bindPopup("<h2>" + feature.properties.AREA_NAME + "</h2>")
+    }
+  }).addTo(map);
+});
